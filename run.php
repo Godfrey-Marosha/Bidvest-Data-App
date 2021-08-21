2d13389c761f1ecf12c048e00e912077e3726b3d
Godfrey Marosha <godfreymarosha26@gmail.com>
2021-08-21 22:54:48 +0200

    Update run.php
    Application operations and/or action commands.

--- run.php
+++ run.php
@@ -1,8 +1,22 @@
 #!php
 <?php
+include_once 'classes\Student.php';
+
+const ACTION_COMMAND = "action";
+const ACTION_ID = "id";
 
 // App operations
 const ACTION_INSERT = "add";
 const ACTION_EDIT   = "edit";
 const ACTION_DELETE = "delete";
 const ACTION_SEARCH = "search";
+
+const PROJECT_DIR = "students";
+
+$defaultValues = array(ACTION_COMMAND => "", ACTION_ID => "", ACTION_SEARCH => "");
+$givenArguments = getopt("", array(ACTION_COMMAND . ":", ACTION_ID . ":", ACTION_SEARCH . ":"));
+$options = array_merge($defaultValues, $givenArguments);
+
+$actionInput = $options[ACTION_COMMAND];
+$actionId = $options[ACTION_ID];
+
