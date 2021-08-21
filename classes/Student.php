d4bd77fa5de5815484d3f0cd4c505766f271216a
Godfrey Marosha <godfreymarosha26@gmail.com>
2021-08-21 22:57:13 +0200

    Update Student.php
    Student constructor.

--- classes/Student.php
+++ classes/Student.php
@@ -8,4 +8,21 @@ class Student
     private $student_surname;
     private $student_age;
     private $student_curriculum;
+
+    /**
+     * Student constructor.
+     * @param $student_id
+     * @param $student_name
+     * @param $student_surname
+     * @param $student_age
+     * @param $student_curriculum
+     */
+    public function __construct($student_id, $student_name, $student_surname, $student_age, $student_curriculum)
+    {
+        $this->student_id = $student_id;
+        $this->student_name = $student_name;
+        $this->student_surname = $student_surname;
+        $this->student_age = $student_age;
+        $this->student_curriculum = $student_curriculum;
+    }
 }
