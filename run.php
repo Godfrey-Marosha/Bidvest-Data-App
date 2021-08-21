3218f1c461c43149f58b5eecb5decc2eb4c51d01
Godfrey Marosha <godfreymarosha26@gmail.com>
2021-08-21 23:18:59 +0200

    Update run.php
    UPDATE: Edit an existing student (--action=edit --id=1234567)

--- run.php
+++ run.php
@@ -47,3 +47,54 @@ if ($actionInput == ACTION_INSERT){
 
     add($student);
 }
+
+/**	UPDATE: Edit an existing student (--action=edit --id=1234567)
+* Student must be identified by student id for editing.
+*/
+else if ($actionInput == ACTION_EDIT){
+    if ($actionId == ""){
+        echo "Please provide an ID in order to update!";
+    }
+    else{
+        $student_id = $actionId;
+
+        if (!doesStudentExist($student_id)){
+            echo "The Student does not exist. Please use a valid ID or add the student first.";
+        }
+
+        $jsonStudent = get($student_id);
+        $student = jsonToStudent($jsonStudent);
+
+        echo "Please enter the values you want to update. Leave field blank to keep the previous value. \n";
+
+        $student_name       = readInput("Enter Name[" . $student->getStudentName() . "]",true);
+        $student_surname    = readInput("Enter Surname[" . $student->getStudentSurname() . "]",true);
+        $student_age        = readInput("Enter Age[" . $student->getStudentAge() . "]",true);
+        $student_curriculum = readInput("Enter Curriculum[" . $student->getStudentCurriculum() . "]",true);
+
+        if ($student_age != ""){
+            if (!isValueADigit($student_age)){
+                echo "The student age must be numeric!";
+                exit;
+            }
+        }
+
+        //echo $student->getStudentAge();
+
+        if (trim($student_name) != "")
+            $student->setStudentName($student_name);
+
+        if (trim($student_surname) != "")
+            $student->setStudentSurname($student_surname);
+
+        if (trim($student_age) != "")
+            $student->setStudentAge($student_age);
+
+        if (trim($student_curriculum) != "")
+            $student->setStudentCurriculum($student_curriculum);
+
+        SaveToFile($student->getStudentId(), $student->toString());
+
+        echo "Record updated!";
+    }
+}
