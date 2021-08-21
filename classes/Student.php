9342cc8babd0ae0e3d975bb1dabae26b03309121
Godfrey Marosha <godfreymarosha26@gmail.com>
2021-08-21 22:48:19 +0200

    Create Student.php

---
+++ classes/Student.php
@@ -0,0 +1,11 @@
+<?php
+
+class Student
+{
+    // Expected input for adding a student
+    private $student_id;
+    private $student_name;
+    private $student_surname;
+    private $student_age;
+    private $student_curriculum;
+}
